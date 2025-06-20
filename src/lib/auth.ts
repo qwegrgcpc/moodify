a3bd0163fdbfff2713f1e56fddd7910d9a854978
yeee <qwegrgcpc@gmail.com>
2025-06-20 15:40:37 +0800

    fix: accessToken 補上型別

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -21,7 +21,7 @@ export const authOptions: NextAuthOptions = {
             return token;
         },
         async session({ session, token }) {
-            session.accessToken = token.accessToken;
+            session.accessToken = token.accessToken as string | undefined;
             return session;
         },
     },
