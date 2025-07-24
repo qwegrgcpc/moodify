0bb0d0fc21f3fd498feb4965ccb3514916b819ab
yeee <qwegrgcpc@gmail.com>
2025-07-24 17:12:53 +0800

    feat: 新增可用環境變數判斷 youtubeSearch 是否用 mock data
    因 api 有 quota 限制

--- src/hooks/useYoutubeSearch.ts
+++ src/hooks/useYoutubeSearch.ts
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { YouTubeSearchResult } from "@/types/youtube";
+import { mockYoutubeResults as mock } from "@/mocks/youtubeSearch.mock";
 
 export default function useYoutubeSearch() {
     const [results, setResults] = useState<YouTubeSearchResult[]>([]);
@@ -7,6 +8,13 @@ export default function useYoutubeSearch() {
 
     const fetchMusic = async (query: string) => {
         setLoading(true);
+
+        if (process.env.NEXT_PUBLIC_USE_MOCK === "true") {
+            setResults(mock);
+            setLoading(false);
+            return;
+        }
+
         try {
             const res = await fetch('/api/youtube', {
                 method: 'POST',
---
+++ src/mocks/youtubeSearch.mock.ts
@@ -0,0 +1,74 @@
+import { YouTubeSearchResult } from "@/types/youtube";
+
+export const mockYoutubeResults: YouTubeSearchResult[] = [
+    {
+        "id": "jfKfPfyJRdk",
+        "title": "lofi hip hop radio 📚 beats to relax/study to",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/jfKfPfyJRdk/default_live.jpg",
+        "url": "https://www.youtube.com/watch?v=jfKfPfyJRdk"
+    },
+    {
+        "id": "sF80I-TQiW0",
+        "title": "90&#39;s Chill Lofi ☕️ Study Music Lofi Rain Chillhop Beats ☔️ Lofi Rain Playlist",
+        "channel": "The Japanese Town",
+        "thumbnail": "https://i.ytimg.com/vi/sF80I-TQiW0/default.jpg",
+        "url": "https://www.youtube.com/watch?v=sF80I-TQiW0"
+    },
+    {
+        "id": "n61ULEU7CO0",
+        "title": "Best of lofi hip hop 2021 ✨ [beats to relax/study to]",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/n61ULEU7CO0/default.jpg",
+        "url": "https://www.youtube.com/watch?v=n61ULEU7CO0"
+    },
+    {
+        "id": "IxPANmjPaek",
+        "title": "medieval lofi radio 🏰 - beats to scribe manuscripts to",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/IxPANmjPaek/default_live.jpg",
+        "url": "https://www.youtube.com/watch?v=IxPANmjPaek"
+    },
+    {
+        "id": "P6Segk8cr-c",
+        "title": "sad lofi radio ☔ beats for rainy days",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/P6Segk8cr-c/default_live.jpg",
+        "url": "https://www.youtube.com/watch?v=P6Segk8cr-c"
+    },
+    {
+        "id": "1oDrJba2PSs",
+        "title": "Study With Me 📚 Pomodoro",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/1oDrJba2PSs/default_live.jpg",
+        "url": "https://www.youtube.com/watch?v=1oDrJba2PSs"
+    },
+    {
+        "id": "S_MOd40zlYU",
+        "title": "dark ambient radio 🌃 music to escape/dream to",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/S_MOd40zlYU/default_live.jpg",
+        "url": "https://www.youtube.com/watch?v=S_MOd40zlYU"
+    },
+    {
+        "id": "VUQfT3gNT3g",
+        "title": "𝙻𝚘𝚏𝚒 𝚁𝚘𝚘𝚖 / 𝙶𝚞𝚒𝚝𝚊𝚛 𝙻𝚘𝚏𝚒 / 𝙲𝚊𝚏𝚎 𝙼𝚞𝚜𝚒𝚌 / 𝙴𝚊𝚜𝚢 𝚕𝚒𝚜𝚝𝚎𝚗𝚒𝚗𝚐 / 𝙱𝙶𝙼 / 𝙻𝚘𝚏𝚒 𝙼𝚞𝚜𝚒𝚌 / 𝚅𝚎𝚛.𝟾",
+        "channel": "Myour Music",
+        "thumbnail": "https://i.ytimg.com/vi/VUQfT3gNT3g/default.jpg",
+        "url": "https://www.youtube.com/watch?v=VUQfT3gNT3g"
+    },
+    {
+        "id": "8uTCoXsOjtk",
+        "title": "Cơ Hội Cuối , Nước Mắt Anh Lại Rơi Vì Em Đấy Lofi TikTok - Nhạc Chill Buồn - Nhạc Chill Dễ Ngủ 2025",
+        "channel": "𝗣𝗿𝗲𝘀𝗲𝗻𝘁?",
+        "thumbnail": "https://i.ytimg.com/vi/8uTCoXsOjtk/default.jpg",
+        "url": "https://www.youtube.com/watch?v=8uTCoXsOjtk"
+    },
+    {
+        "id": "lTRiuFIWV54",
+        "title": "1 A.M Study Session 📚 [lofi hip hop]",
+        "channel": "Lofi Girl",
+        "thumbnail": "https://i.ytimg.com/vi/lTRiuFIWV54/default.jpg",
+        "url": "https://www.youtube.com/watch?v=lTRiuFIWV54"
+    }
+]
