ca0305426bcd295b830f40a895d585ae4fd1f996
yeee <qwegrgcpc@gmail.com>
2025-07-24 15:01:03 +0800

    feat: 加入 header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,14 @@
 'use client';
 
 import { SessionProvider } from "next-auth/react";
+import Header from "@/components/Header";
 import "@/styles/globals.css";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+        <Header/>
         <SessionProvider>{children}</SessionProvider>
       </body>
     </html>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
-import LoginButton from "@/components/LoginButton";
+"use client";
+import { useRouter } from "next/navigation";
+// import LoginButton from "@/components/LoginButton";
 
 export default function Header() {
+  const router = useRouter();
+
   return (
     <header className="p-4 border-b flex justify-between">
-      <h1 className="text-lg font-bold">🎧 Moodify</h1>
-      <LoginButton />
+      <h1
+        className="text-lg font-bold cursor-pointer"
+        onClick={() => router.push("/")}
+      >
+        Moodify
+      </h1>
+      {/* <LoginButton /> */}
     </header>
   );
 }
